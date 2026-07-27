e60623e3ac75896b9a27fe1361e7f8f2cea47460
JKawari <akramulraza123@gmail.com>
2026-07-27 16:44:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   console.log("Welcome to Adhyanpur Premier League!");
+
+  const cards = document.querySelectorAll(
+    ".team-card, .match-card, .award-card, .news-card"
+  );
+
+  cards.forEach(card => {
+    card.addEventListener("click", () => {
+      card.style.transform = "scale(1.03)";
+      setTimeout(() => {
+        card.style.transform = "scale(1)";
+      }, 200);
+    });
+  });
 });
